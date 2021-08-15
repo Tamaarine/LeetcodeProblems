e74dd355821341e85fde1224c9c080759ecc9bdf
Tamaarine <irebootplaygt@gmail.com>
2021-08-15 14:35:29 -0400

    Still working on longest palindrome

--- _5/LongestPalindrome.java
+++ _5/LongestPalindrome.java
@@ -2,17 +2,24 @@ package _5;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
+        System.out.println(longestPalindrome("racecar"));
+        System.out.println(longestPalindrome("ac"));
     }
     
     public static String longestPalindrome(String s) {
+        if(s.length() <= 1) {
+            return s;
+        }
         String max = "";
         for(int i=0;i<s.length();i++) {
-            for(int j=i+1;j<s.length();j++) {
+            for(int j=i;j<s.length();j++) {
                 String sub = s.substring(i, j + 1);
-                if(isPalindrome(s.substring(i, j + 1)) && )
+                if(isPalindrome(sub) && sub.length() > max.length()) {
+                    max = sub;
+                }
             }
         }
+        return max;
     }
     
     public static boolean isPalindrome(String s) {
